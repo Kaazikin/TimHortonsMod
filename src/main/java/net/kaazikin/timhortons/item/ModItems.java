661d8e3351bc0533cb2517fa9212a4de7bc1d842
Tyler Campbell <eatmybubbles8400@gmail.com>
2023-02-26 22:26:29 -0500

    Updated textures for timbits Added recipes for timbits

--- src/main/java/net/kaazikin/timhortons/item/ModItems.java
+++ src/main/java/net/kaazikin/timhortons/item/ModItems.java
@@ -1,6 +1,7 @@
 package net.kaazikin.timhortons.item;
 
 import net.kaazikin.timhortons.TimHortonsMod;
+import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -13,10 +14,10 @@ public class ModItems {
             DeferredRegister.create(ForgeRegistries.ITEMS, TimHortonsMod.MOD_ID);
 
     public static final RegistryObject<Item> TIMBIT = ITEMS.register("timbit",
-            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD)));
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD).food(new FoodProperties.Builder().nutrition(2).saturationMod(0.5F).build())));
 
     public static final RegistryObject<Item> BIRTHDAY_TIMBIT = ITEMS.register("birthday_timbit",
-            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD)));
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD).food(new FoodProperties.Builder().nutrition(4).saturationMod(0.8F).build())));
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
     }
