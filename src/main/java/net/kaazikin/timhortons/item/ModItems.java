cda9102432c7774f8b2d000d6ccc41adb0c185df
Tyler Campbell <eatmybubbles8400@gmail.com>
2023-03-01 21:51:17 -0500

    Updated to 0.0.2-1.19.2 Fixed chocolate timbit Added turtle timbit

--- src/main/java/net/kaazikin/timhortons/item/ModItems.java
+++ src/main/java/net/kaazikin/timhortons/item/ModItems.java
@@ -1,9 +1,12 @@
 package net.kaazikin.timhortons.item;
 
 import net.kaazikin.timhortons.TimHortonsMod;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Rarity;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -19,8 +22,14 @@ public class ModItems {
     public static final RegistryObject<Item> BIRTHDAY_TIMBIT = ITEMS.register("birthday_timbit",
             () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD).food(new FoodProperties.Builder().nutrition(4).saturationMod(0.8F).build())));
 
-    public static final RegistryObject<Item> CHOCOLATE_TIMBIT = ITEMS.register("birthday_timbit",
+    public static final RegistryObject<Item> CHOCOLATE_TIMBIT = ITEMS.register("chocolate_timbit",
             () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD).food(new FoodProperties.Builder().nutrition(4).saturationMod(1F).build())));
+
+    public static final RegistryObject<Item> TURTLE_TIMBIT = ITEMS.register("turtle_timbit",
+            () -> new Item(new Item.Properties().fireResistant().stacksTo(1).tab(CreativeModeTab.TAB_FOOD).food
+                            (new FoodProperties.Builder().nutrition(10).saturationMod(10F).alwaysEat().
+                                    effect(() -> new MobEffectInstance(MobEffects.DAMAGE_RESISTANCE, 800, 4), 0.7F).build())
+                    .rarity(Rarity.RARE)));
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
